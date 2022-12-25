8ace7df9120e2bde7819aeca0568fe13f9c17fa4
Carlos Cristobal <sw.carlos.cristobal@gmail.com>
2022-12-25 01:25:36 -0700

    push

--- src/parseLinkedIn.js
+++ src/parseLinkedIn.js
@@ -1,12 +1,16 @@
 // Make a GET request to your LinkedIn profile page
-axios
-  .get({
-    URL: 'https://www.linkedin.com/in/carloscristobal/',
-    Headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  })
-  .then((response) => {
+debugger;
+axios({
+  method: 'get',
+  url: 'https://www.linkedin.com/in/carloscristobal/',
+  headers: {
+    //'Access-Control-Allow-Origin': 'http://localhost:5500',
+    //'referer': 'https://www.google.com/',
+    // Useragent header for googlebot
+    'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+    
+  },
+}).then((response) => {
     // Parse the HTML of the page
     const html = response.data;
     const parser = new DOMParser();
